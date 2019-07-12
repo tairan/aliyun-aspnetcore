be8ddd032cb02cc5cce84e519ab655a7aa71a43a
Tairan Wang <tairan.wang@gmail.com>
2019-07-12 23:16:49 +0800

    Try to support TAP from APM

---
+++ src/AliyunOss.AspNetCore/Aliyun/OSS/IOssClientExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aliyun.OSS
+{
+    public static class IOssClientExtensions
+    {
+        public static Task<OssObject> GetObjectAsync(this IOss client, string bucketName, string key)
+        {
+            return Task.Factory.FromAsync(client.BeginGetObject, client.EndGetObject, bucketName, key, null);
+        }
+
+        public static Task<AppendObjectResult> AppendObjectAsync(this IOss client, AppendObjectRequest request)
+        {
+            return Task.Factory.FromAsync(client.BeginAppendObject, client.EndAppendObject, request, null);
+        }
+    }
+}
